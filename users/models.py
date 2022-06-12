414ee76e35e8b590c91b1fa8bee534740656bdcf
ludwig <murimiludwig@gmail.com>
2022-06-12 22:25:06 +0300

    creation of the profile update form

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg',upload_to='profile_pics')
-    bio = models.CharField(max_length=200)
+    bio = models.CharField(max_length=500)
     
     
     def __str__(self, ):
