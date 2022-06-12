9c5daf4e905d871836d0712e61c9f46355e92320
ludwig <murimiludwig@gmail.com>
2022-06-12 17:25:42 +0300

    creation and modification of the user models

--- my_awards/settings.py
+++ my_awards/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awards.apps.AwardsConfig',
-    'users.apps.UserConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
