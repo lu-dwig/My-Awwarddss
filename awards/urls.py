9701618349e9840f624de056ca224b904900cc09
ludwig <murimiludwig@gmail.com>
2022-06-13 14:49:28 +0300

    renaming of the setting html

--- awards/urls.py
+++ awards/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
+    path('search/', views.searchPhoto, name='search'),
+    
     
 ]
