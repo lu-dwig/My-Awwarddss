5414c2ccaf2c9a939c5e73eaff716143b1736c3f
ludwig <murimiludwig@gmail.com>
2022-06-13 09:27:33 +0300

    creation and modification of the user home page

--- awards/views.py
+++ awards/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 from .models import Post
-from users.forms import 
+from users.forms import  PostForm
+from pyuploadcare.dj.forms import ImageField
+import random
+
 # Create your views here.
+def home(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        post = posts
+        posts = Post.objects.all()
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    # try:
+        # posts = Post.objects.all()
+        # posts = posts[::-1]
+        # x = len(posts)-1
+        # a_post = random.randint(1, 6)
+        # random_post = posts[a_post]
+        # print(random_post.photo)
+    # except Post.DoesNotExist:
+        # posts = None
+    return render(request, 'awards/home.html',  {'form': form})
 
