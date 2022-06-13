990a8938796bf1168ee9afc5c6b32ae4d2ac710d
ludwig <murimiludwig@gmail.com>
2022-06-13 12:09:00 +0300

    modification of the urlss and othe r files

--- api/views.py
+++ api/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import 
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 # Create your views here.
 
+@api_view(['GET'])
 def apiOverview(request):
-    return
+    api_urls = {
+        'PostList':'/post-list/',
+        'Detail View': '/post-detail/<str:pk>/',
+        'Create': '/post-create/',
+        'Update': '/post-update/<str:pk>/',
+        'Delete': '/post-delete/<str:pk>/',
+    }
+    return Response(api_urls)
--- awards/models.py
+++ awards/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     description = models.TextField(max_length=255)
     technologies = models.CharField(max_length=200, blank=True)
     image = models.ImageField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
