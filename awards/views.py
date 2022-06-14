c92a34d5050ea912cfb58e317858fcad8faadf0a
ludwig <murimiludwig@gmail.com>
2022-06-14 11:57:40 +0300

    creation of the post update view

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Post, Ratings
 from users.forms import  PostForm, RatingsForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
@@ -94,3 +96,35 @@ def create(request):
     print(res)
     messages.success(request, 'project deleted successfully!')
     return redirect('home')
+
+# @login_required(login_url='login')
+# def upload(request):
+#     post = Post.objects.get(user=request.user)
+#     postimage = post.profile_pic.url
+#     if request.method == 'POST':
+#         post = request.FILES['post']
+#         print(post)
+#         post = Post.objects.get(user=request.user)
+#         posts = Post.objects.create(user=request.user,photo=post)
+#         if posts:
+#             messages.success(request,"post uploaded successfully!")
+#         else:
+#             messages.success(request,"post failed!")
+#     return render(request,'awards/uploadpost.html',{'profileimage':profileimage})
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin ,UpdateView):
+    model= Post
+    fields = ['image', 'title',  'desccription', 'url']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
