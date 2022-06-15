f8e42154f06a645916dcb58bdad4f47b969a5c5c
ludwig <murimiludwig@gmail.com>
2022-06-15 08:48:25 +0300

    heroku deployment

---
+++ staticfiles/awards/buildpost.b366fc511c90.js
@@ -0,0 +1,39 @@
+buildPost()
+
+function buildPost() {
+    var wrapper = document.getElementById('Post-wrapper')
+    var url = 'http://127.0.0.1:8000/api/post-list/'
+
+    fetch(url)
+    .then((res) => res.json())
+    .then(function(data){
+       console.log(data)
+       var list = data
+
+       for (let i in list) {
+        let awards = `
+            <div class="col-md-6 col-lg-4">
+                <div class="card my-2">
+                    <img style='height: 250px;object-fit:cover;' src="${list[i].image}" class="card-img-top" alt="photo image">
+                    <div class="card-body">
+                        <p class="card-text">Title: ${list[i].title}</p>
+                    </div>
+                    <a href="detail/1/" class="btn btn-outline-dark btn-sm view-btn">view</a>
+                </div>
+            </div>
+        `
+        wrapper.innerHTML += awards
+    }
+    for (var i in list) {
+        var viewBtn = document.getElementsByClassName('view-btn')[i]
+
+        viewBtn.addEventListener('click', function(){
+            console.log(list[i].id)
+        })
+
+    }
+
+ })
+}
+
+
